1a6e9235f1d7c5df68bebb30a9972b1c2124e8fb
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-09-22 20:19:46 +0200

    Add chrome-extension build process

---
+++ chrome-extension/config/webpack.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+  entry: {
+    content: path.join(__dirname, '..', 'src', 'content.js'),
+    background: path.join(__dirname, '..', 'src', 'background.js')
+  },
+  output: {
+    path: path.join(__dirname, '..', 'build'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      {
+        test: /\.js$/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          babelrc: false,
+          presets: [require.resolve('babel-preset-react-app')],
+          compact: true
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(
+          Object.assign(
+            {
+              fallback: require.resolve('style-loader'),
+              use: [
+                {
+                  loader: require.resolve('css-loader'),
+                  options: {
+                    importLoaders: 1,
+                    minimize: true,
+                    sourceMap: true
+                  }
+                },
+                {
+                  loader: require.resolve('postcss-loader'),
+                  options: {
+                    ident: 'postcss',
+                    plugins: () => [
+                      require('postcss-flexbugs-fixes'),
+                      autoprefixer({
+                        browsers: [
+                          '>1%',
+                          'last 4 versions',
+                          'Firefox ESR',
+                          'not ie < 9' // React doesn't support IE8 anyway
+                        ],
+                        flexbox: 'no-2009'
+                      })
+                    ]
+                  }
+                }
+              ]
+            },
+            {}
+          )
+        )
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new ExtractTextPlugin({
+      filename: 'style.css'
+    }),
+    new CopyWebpackPlugin(
+      [
+        { from: 'assets', to: 'assets' },
+        { from: 'manifest.json', to: 'manifest.json' }
+      ],
+      {}
+    )
+  ].concat(
+    isProduction
+      ? [
+          new webpack.optimize.UglifyJsPlugin({
+            compress: {
+              warnings: false,
+              comparisons: false
+            },
+            output: {
+              comments: false,
+              ascii_only: true
+            },
+            sourceMap: true
+          })
+        ]
+      : []
+  )
+};
