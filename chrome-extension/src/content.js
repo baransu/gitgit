ba6e23517f3dce221e59a555c9bca1486b9d134c
Krzysztof Wende <krzysztof.wende@gmail.com>
2017-09-23 16:38:52 +0200

    memo #1

--- chrome-extension/src/content.js
+++ chrome-extension/src/content.js
@@ -1,52 +1,26 @@
-// import React from 'react';
-// import { render } from 'react-dom';
-
-import { get } from './api';
+// import { get } from './api';
 import './style.scss';
 
-// We want to match both lowercase strings and their capitalized versions in pages
+console.log('GitGit initialized');
 
-// the text to use as content--the inner html of the page
-// document.body.innerHTML = replaceBoringPhrases(document.body.innerHTML);
+const getUser = memoize(10000, function getUser (user, f){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.github.com/users/" + user + "/repos", true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      // JSON.parse does not evaluate the attacker's scripts.
+      var resp = JSON.parse(xhr.responseText);
+      f(resp);
+    }
+  };
+  xhr.send();
+});
 
-// console.log('HA');
-// console.log(chrome);
-
-document.addEventListener('pjax:end', run);
-run();
-
-function run() {
-  // run code/call function
-  // const users = {
-  //   wende: "Elixir Alchemist Lvl 10 </br> Elm Druid Lvl 4",
-  //   Baransu: "JS Ninja Lvl 10 </br> Elm Druid Lvl 4"
-  // };
-
-  // chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-  const url = window.location.href;
-  //for(user in users){
-  //if(url.indexOf(user) !== -1){
-  const user = window.location.href.split('/')[3];
-  const pageUser = document.querySelector('.p-nickname.vcard-username.d-block');
-  let commentUser = document.querySelector(
-    '.avatar-parent-child.timeline-comment-avatar'
-  );
-  if (pageUser) {
-    getUser(user, repos => {
-      const level = getLevels(repos);
-      pageUser.innerHTML += '</br>' + level;
-    });
-  }
-  if (commentUser) {
-    commentUser.innerHTML += "<div class='ribbon'></div>";
-  }
-}
-
-function getUser(user, cb) {
-  get(`/users/${user}/repos`)
-    .then(res => cb(res.data))
-    .catch(console.error);
-}
+// const getUser = memoize(10000, function getUser(user, cb) {
+//   get(`/users/${user}/repos`)
+//     .then(res => cb(res.data))
+//     .catch(console.error);
+// });
 
 const groupBy = function(xs, key) {
   return xs.reduce((acc, x) => {
@@ -88,3 +62,47 @@ function getClass(language) {
   }[language];
   return c ? `${c} (${language})` : language;
 }
+
+
+function memoize(t, name, f){
+  return function(...args) {
+    let name = name + JSON.stringify(args);
+    chrome.storage.sync.get(name, (res) => {
+      let now = Date.now();
+      console.log(res)
+      let v = (res == {}) ? {} : JSON.parse(res);
+      if(!v || now - v.time > t) {
+        console.log("memoizing");
+        v = {time: now, value: f.apply(args)};
+        chrome.storage.sync.set({[name]: JSON.stringify(v)}, () => ({}));
+      }
+      console.log("memoized");
+      return v.value;
+    });
+  };
+}
+
+function run() {
+  const url = window.location.href;
+  const user = window.location.href.split('/')[3];
+  const pageUser = document.querySelector('.p-nickname.vcard-username.d-block');
+  let commentUser = document.querySelector(
+    '.avatar-parent-child.timeline-comment-avatar'
+  );
+  if (pageUser) {
+    getUser(user, repos => {
+      const level = getLevels(repos);
+      pageUser.innerHTML += '</br>' + level;
+    });
+  }
+  if (commentUser) {
+    commentUser.innerHTML += "<div class='ribbon'></div>";
+  }
+}
+
+///////////////////////////
+// ------- main -------- //
+///////////////////////////
+
+document.addEventListener('pjax:end', run);
+run();
